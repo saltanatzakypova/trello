457a5727b3bcddadda16fc1ea6c0a36f31c11c91
Saltanat <zakypovasaltanat6@gmail.com>
2023-06-25 12:13:17 +0600

    theme

--- src/containers/Provider.jsx
+++ src/containers/Provider.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "../store";
+import Theme from "../theme";
+import { CssBaseline } from "@mui/material";
 
 const Providers = ({ children }) => {
   return (
     <Providers store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {children}
+        <Theme>
+          <CssBaseline />
+          {children}
+        </Theme>
       </PersistGate>
     </Providers>
   );
